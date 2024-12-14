16457170bfbdcad90665ad2109422ed06e2c71e7
quantum-ernest <easare@quantumgroupgh.com>
2024-12-14 19:15:28 +0000

    api: add environment variables

---
+++ api/core/env_config.py
@@ -0,0 +1,23 @@
+from pydantic import EmailStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class EnvConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB_NAME: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    AUTH_SECRETE_KEY: str
+    AUTH_ALGORITHM: str
+    EMAIL_SERVER_HOST: str
+    EMAIL_SERVER_PORT: int
+    EMAIL_SERVER_USE_TLS: bool
+    EMAIL_SERVER_ADDRESS: EmailStr
+    EMAIL_HOST_PASSWORD: str
+    REDIS_HOST: str | None = "localhost"
+    REDIS_PORT: int | None = "6379"
+
+
+env = EnvConfig()
