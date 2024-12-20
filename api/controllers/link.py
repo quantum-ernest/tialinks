adcf9e6e56fa49b022bf58999ac5d661e0aabb08
quantum-ernest <easare@quantumgroupgh.com>
2024-12-20 21:49:05 +0000

    api: extract utm field from url

--- api/controllers/link.py
+++ api/controllers/link.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from core import get_db_session
 from typing import List
-from models import LinkMapper
+from models import LinkMapper, UtmMapper
 from schemas import LinkSchemaOut, LinkSchemaIn
 from services import IsAuthenticated
-from utils import generate_readable_short_code
+from utils import generate_readable_short_code, extract_utm_data
 
 router = APIRouter(prefix="/api/link", tags=["LINK"])
 
@@ -36,13 +36,31 @@ async def create(
         else:
             shortcode = new_shortcode
             break
+    original_url = str(data.get("original_url"))
+    utm_data = extract_utm_data(original_url)
+    utm = UtmMapper.validate_utm(
+        session=session,
+        campaign=utm_data.get("utm_campaign"),
+        source=utm_data.get("utm_source"),
+        user_id=auth_user.get("user_id"),
+    )
+    if not utm:
+        utm = UtmMapper.create(
+            session=session,
+            data={
+                "campaign": utm_data.get("utm_campaign"),
+                "source": utm_data.get("utm_source"),
+                "medium": utm_data.get("utm_medium"),
+                "user_id": auth_user.get("user_id"),
+            },
+        )
 
     data.update(
         {
-            "original_url": str(data.get("original_url")),
+            "original_url": original_url,
             "shortcode": shortcode,
+            "utm_id": utm.id if utm else None,
             "user_id": auth_user.get("user_id"),
         }
     )
-
     return LinkMapper.create(session=session, data=data)
