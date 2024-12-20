c5bf2c15474bd3405ee6c5310a138927bf7e4935
quantum-ernest <easare@quantumgroupgh.com>
2024-12-20 21:49:24 +0000

    api: add utm routes

---
+++ api/controllers/utm.py
@@ -0,0 +1,67 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from typing import List
+from core import get_db_session
+from models import UtmMapper
+from schemas import UtmSchemaOut, UtmSchema
+from services import IsAuthenticated
+
+router = APIRouter(prefix="/api/utm", tags=["UTM"])
+
+
+@router.get("", response_model=List[UtmSchemaOut])
+async def get(
+    session: Session = Depends(get_db_session),
+    auth_user: dict = Depends(IsAuthenticated()),
+):
+    return UtmMapper.get_all(session=session, user_id=auth_user.get("user_id"))
+
+
+@router.post("", response_model=UtmSchemaOut)
+async def create(
+    data: UtmSchema,
+    session: Session = Depends(get_db_session),
+    auth_user: dict = Depends(IsAuthenticated()),
+):
+    utm = UtmMapper.validate_utm(
+        session=session,
+        campaign=data.campaign,
+        source=data.source,
+        user_id=auth_user.get("user_id"),
+    )
+    if utm:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Utm already exists"
+        )
+    data = data.model_dump()
+    data.update({"user_id": auth_user.get("user_id")})
+    return UtmMapper.create(session=session, data=data)
+
+
+@router.put("/{id}", response_model=UtmSchemaOut)
+async def update(
+    id: int,
+    data: UtmSchema,
+    session: Session = Depends(get_db_session),
+    auth_user: dict = Depends(IsAuthenticated()),
+):
+    utm = UtmMapper.get_by_id(
+        session=session, pk_id=id, user_id=auth_user.get("user_id")
+    )
+    if not utm:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Utm not found"
+        )
+    val_utm = UtmMapper.validate_utm(
+        session=session,
+        campaign=data.campaign,
+        source=data.source,
+        user_id=auth_user.get("user_id"),
+    )
+    if val_utm:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Utm already exists"
+        )
+    data = data.model_dump()
+    data.update({"id": id, "user_id": auth_user.get("user_id")})
+    return UtmMapper.update(session=session, data=data)
