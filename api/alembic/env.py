a44a68862051c069664304517b33298e29b2957b
quantum-ernest <easare@quantumgroupgh.com>
2024-12-23 21:22:01 +0000

    api: add link interaction migration

--- api/alembic/env.py
+++ api/alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 from core import DATABASE_URL
-from models import Base, ClickMapper, UtmMapper, LinkMapper, LocationMapper, UserMapper, UserAgentMapper, RefererMapper
+from models import Base, ClickMapper, UtmMapper, LinkMapper, LocationMapper, UserMapper, UserAgentMapper, RefererMapper, LinkInteractionMapper
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
