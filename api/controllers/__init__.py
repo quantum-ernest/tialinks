9fb1caa0a92ed980d47ba408ff28f83dd56029f6
quantum-ernest <easare@quantumgroupgh.com>
2024-12-24 20:52:22 +0000

    api: update controller import

--- api/controllers/__init__.py
+++ api/controllers/__init__.py
@@ -3,3 +3,4 @@ from .user import router as user_router
 from .link import router as link_router
 from .click import router as click_router
 from .utm import router as utm_router
+from .link_interactions import router as analysis_router
