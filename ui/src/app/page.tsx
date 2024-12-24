510c7609f2e56680774191d7cf7ea53a2fb11e4a
quantum-ernest <easare@quantumgroupgh.com>
2024-12-24 18:42:27 +0000

    ui: add home page

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,13 +1,19 @@
+"use client"
 import styles from "./page.module.css";
 import RequestOTPForm from "@/components/RequestOTPForm";
+import OTPLoginForm from "@/components/OTPLoginForm";
+import {useState} from "react";
 
 export default function Home() {
+    const [email, setEmail] = useState("");
+
     return (
         <div className={styles.page} style={{
             backgroundImage: `url("/background.jpg")`, backgroundSize: "cover", backgroundRepeat: 'no-repeat',
             backgroundPosition: 'center', width: '100%', height: '100%',
         }}>
-            <RequestOTPForm/>
+            <RequestOTPForm setEmail={setEmail} />
+            {email && <OTPLoginForm email={email}/>}
         </div>
     );
 }
