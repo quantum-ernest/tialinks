b6005304680ac187f31fac4e0c5e4c24499fc00c
quantum-ernest <easare@quantumgroupgh.com>
2024-12-25 17:35:37 +0000

    ui: add error display and otp type

--- ui/src/components/OTPLoginForm.tsx
+++ ui/src/components/OTPLoginForm.tsx
@@ -1,14 +1,22 @@
 "use client"
 import {Form} from "antd"
 import {InputOTP} from 'antd-input-otp';
+import {Email} from "@/components/types";
+import {useState} from "react";
 
-function OTPLoginForm({email}: { email: string }) {
+type OTP = {
+    otp: string[] | null
+}
+
+function OTPLoginForm({email}: Email) {
     const [form] = Form.useForm();
-    const handleFinish = async (values: any | null) => {
+    const [error, setError] = useState<string|null>();
+    const handleFinish = async (values: OTP) => {
         const apiUrl = process.env.NEXT_PUBLIC_BACKEND_BASE_URL
-        const otp = values?.otp.join("")
+        const otp = values?.otp?.join("")
         if (!otp) {
-            return;
+            setError("Invalid OTP");
+            return
         }
         try {
             const response = await fetch(apiUrl + '/api/auth/otp/email/login', {
@@ -21,7 +29,13 @@ function OTPLoginForm({email}: { email: string }) {
             }
             const data = await response.json()
             localStorage.setItem("access_token", data.token);
+            setError(null)
         } catch (error) {
+            if (error instanceof Error ){
+                setError(error.message);
+            }else{
+                setError("An unexpected error occurred");
+            }
             console.error(error)
         }
     };
@@ -31,6 +45,7 @@ function OTPLoginForm({email}: { email: string }) {
             <Form.Item label="Enter OTP" name="otp">
                 <InputOTP autoSubmit={form} autoFocus={true} inputType="numeric"/>
             </Form.Item>
+            {error && <p>{error}</p>}
         </Form>
     );
 }
