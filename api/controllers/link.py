111fd25a4b24e32e71080cbee2ddf0c20f30064c
quantum-ernest <ernestasare2411@gamil.com>
2025-01-06 23:31:37 +0000

    api: add update link route

--- api/controllers/link.py
+++ api/controllers/link.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from core import get_db_session
 from typing import List
 from models import LinkMapper, UtmMapper
-from schemas import LinkSchemaOut, LinkSchemaIn
+from schemas import LinkSchemaOut, LinkSchemaIn, LinkSchemaUpdate
 from services import IsAuthenticated
 from utils import generate_readable_short_code, extract_utm_data
 
@@ -15,7 +15,7 @@ async def get_all(
     session: Session = Depends(get_db_session),
     auth_user: dict = Depends(IsAuthenticated()),
 ):
-    return LinkMapper.get_all(session=session, user_id=auth_user.get("user_id"))
+    return LinkMapper.get_all(session, auth_user.get("user_id"))
 
 
 @router.post("", response_model=LinkSchemaOut)
@@ -64,3 +64,32 @@ async def create(
         }
     )
     return LinkMapper.create(session=session, data=data)
+
+
+@router.put("/{id}", response_model=LinkSchemaOut)
+async def update(
+    id: int,
+    data: LinkSchemaUpdate,
+    session: Session = Depends(get_db_session),
+    auth_user: dict = Depends(IsAuthenticated()),
+):
+    val_link = LinkMapper.get_by_id(session, id, user_id=auth_user["user_id"])
+    if not val_link:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Link not found"
+        )
+    data = data.model_dump()
+    if data.get("utm_id"):
+        val_utm = UtmMapper.get_by_id(
+            session, data["utm_id"], user_id=auth_user["user_id"]
+        )
+        if not val_utm:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Utm not found"
+            )
+    data.update(
+        {
+            "id": id,
+        }
+    )
+    return LinkMapper.update(session, data=data)
