3dbe0d028bbe5abf68bb09fdc83a102cdd01bd86
quantum-ernest <ernestasare2411@gamil.com>
2025-01-06 22:27:44 +0000

    api: update analytics route

--- api/controllers/link_interactions.py
+++ api/controllers/link_interactions.py
@@ -1,13 +1,16 @@
+from datetime import datetime
+from typing import Optional
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from core import get_db_session
 from models import LinkInteractionMapper, LinkMapper
 from services import IsAuthenticated
 
-router = APIRouter(prefix="/api/analysis", tags=["ANALYSIS"])
+router = APIRouter(prefix="/api/analytics", tags=["ANALYSIS"])
 
 
-@router.get("/dashboard/summary")
+@router.get("/dashboard")
 async def get_dashboard_summary(
     session: Session = Depends(get_db_session),
     auth_user: dict = Depends(IsAuthenticated()),
@@ -39,7 +42,7 @@ async def get_dashboard_summary(
         "top_device": LinkInteractionMapper.get_top_devices(
             session=session, user_id=auth_user["user_id"]
         ),
-        "top_country": LinkInteractionMapper.get_top_country(
+        "top_country": LinkInteractionMapper.get_geo_summary_data(
             session=session, user_id=auth_user["user_id"]
         ),
         "monthly_click_trend": LinkInteractionMapper.get_monthly_click_trend(
@@ -49,31 +52,87 @@ async def get_dashboard_summary(
     return result
 
 
-@router.get("/summary")
-async def get_summary(
+@router.get("")
+async def get_analytics(
+    start_date: datetime,
+    end_date: datetime,
+    link_id: Optional[int] = None,
     session: Session = Depends(get_db_session),
     auth_user: dict = Depends(IsAuthenticated()),
 ):
-    return LinkInteractionMapper.get_total_clicks(
-        session=session, user_id=auth_user["user_id"]
+    total_clicks = LinkInteractionMapper.get_total_clicks(
+        session=session,
+        user_id=auth_user["user_id"],
+        start_date=start_date,
+        end_date=end_date,
+        link_id=link_id,
     )
-
-
-@router.get("/trend")
-async def get_trend(
-    session: Session = Depends(get_db_session),
-    auth_user: dict = Depends(IsAuthenticated()),
-):
-    return LinkInteractionMapper.get_clicks_trend_by_day(
-        session=session, user_id=auth_user["user_id"]
+    total_links = (
+        1
+        if link_id
+        else LinkMapper.get_total_links(
+            session=session,
+            user_id=auth_user["user_id"],
+            start_date=start_date,
+            end_date=end_date,
+        )
     )
-
-
-@router.get("/trend/country")
-async def get_trend_country(
-    session: Session = Depends(get_db_session),
-    auth_user: dict = Depends(IsAuthenticated()),
-):
-    return LinkInteractionMapper.get_clicks_by_country(
-        session=session, user_id=auth_user["user_id"]
-    )
+    result = {
+        "total_links": total_links,
+        "total_clicks": total_clicks,
+        "top_performing_links": LinkInteractionMapper.get_top_performing_links(
+            session=session,
+            user_id=auth_user["user_id"],
+            start_date=start_date,
+            end_date=end_date,
+        ),
+        "total_clicks_per_day": LinkInteractionMapper.get_total_clicks_per_day(
+            session=session,
+            user_id=auth_user["user_id"],
+            start_date=start_date,
+            end_date=end_date,
+        ),
+        "referring_campaigns": LinkInteractionMapper.get_top_referring_campaign(
+            session=session,
+            user_id=auth_user["user_id"],
+            start_date=start_date,
+            end_date=end_date,
+        ),
+        "referring_sites": LinkInteractionMapper.get_top_referring_site(
+            session=session,
+            user_id=auth_user["user_id"],
+            start_date=start_date,
+            end_date=end_date,
+        ),
+        "devices": LinkInteractionMapper.get_top_devices(
+            session=session,
+            user_id=auth_user["user_id"],
+            start_date=start_date,
+            end_date=end_date,
+        ),
+        "browsers": LinkInteractionMapper.get_top_browsers(
+            session=session,
+            user_id=auth_user["user_id"],
+            start_date=start_date,
+            end_date=end_date,
+        ),
+        "operating_systems": LinkInteractionMapper.get_top_operating_systems(
+            session=session,
+            user_id=auth_user["user_id"],
+            start_date=start_date,
+            end_date=end_date,
+        ),
+        "geographical_data": LinkInteractionMapper.get_geographical_data(
+            session=session,
+            user_id=auth_user["user_id"],
+            start_date=start_date,
+            end_date=end_date,
+        ),
+        "monthly_click_trend": LinkInteractionMapper.get_monthly_click_trend(
+            session=session,
+            user_id=auth_user["user_id"],
+            start_date=start_date,
+            end_date=end_date,
+        ),
+    }
+    return result
