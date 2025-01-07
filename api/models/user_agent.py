191487647ae8ffab5322bf4c1fbaa18ada3f3c58
quantum-ernest <ernestasare2411@gamil.com>
2025-01-07 15:33:21 +0000

    api: refactor create user agent

--- api/models/user_agent.py
+++ api/models/user_agent.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from models import Base
 from sqlalchemy import insert, select
 from sqlalchemy.orm import Mapped, Session, relationship
@@ -5,29 +7,26 @@ from utils import extract_user_agent
 
 
 class UserAgentMapper(Base):
-    user_agent: Mapped[str | None]
-    browser: Mapped[str | None]
-    operating_system: Mapped[str | None]
-    device: Mapped[str | None]
+    user_agent: Mapped[Optional[str]]
+    browser: Mapped[Optional[str]]
+    operating_system: Mapped[Optional[str]]
+    device: Mapped[Optional[str]]
 
     click: Mapped["ClickMapper"] = relationship(back_populates="user_agent")
 
     @classmethod
     def create(cls, session: Session, **kwargs):
-        user_agent_data = kwargs.get("user_agent")
-        user_agent = extract_user_agent(user_agent_data)
-        query = select(cls)
-        if user_agent:
-            if user_agent.get("browser"):
-                query = query.filter_by(browser=user_agent["browser"])
-            if user_agent.get("operating_system"):
-                query = query.filter_by(operating_system=user_agent["operating_system"])
-            if user_agent.get("device"):
-                query = query.filter_by(device=user_agent["device"])
-            record = session.scalars(query).first()
-            return (
-                record
-                if record
-                else session.scalars(insert(cls).returning(cls), user_agent).first()
+        data = kwargs.get("user_agent")
+        user_agent = extract_user_agent(data)
+        record = session.scalars(
+            select(cls).filter_by(
+                browser=user_agent["browser"],
+                operating_system=user_agent["operating_system"],
+                device=user_agent["device"],
             )
-        return None
+        ).first()
+        return (
+            record
+            if record
+            else session.scalars(insert(cls).returning(cls), user_agent).first()
+        )
