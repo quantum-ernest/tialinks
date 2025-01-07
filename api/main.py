df8365d07ae4be5986db1c32deaabd05ca87636f
quantum-ernest <ernestasare2411@gamil.com>
2025-01-07 15:37:06 +0000

    api: add api version

--- api/main.py
+++ api/main.py
@@ -12,6 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="TiaLinks API",
+    version="0.1.0",
     docs_url="/api/docs",
     redoc_url="/api/redoc",
     openapi_url="/api/openapi.json",
