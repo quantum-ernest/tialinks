8d8818e5402e7c6500f452e299ff39ef7de0e57d
quantum-ernest <ernestasare2411@gamil.com>
2025-01-07 15:30:37 +0000

    api: remove validation

--- api/controllers/utm.py
+++ api/controllers/utm.py
@@ -23,16 +23,6 @@ async def create(
     session: Session = Depends(get_db_session),
     auth_user: dict = Depends(IsAuthenticated()),
 ):
-    utm = UtmMapper.validate_utm(
-        session=session,
-        campaign=data.campaign,
-        source=data.source,
-        user_id=auth_user.get("user_id"),
-    )
-    if utm:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Utm already exists"
-        )
     data = data.model_dump()
     data.update({"user_id": auth_user.get("user_id")})
     return UtmMapper.create(session=session, data=data)
@@ -52,15 +42,6 @@ async def update(
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Utm not found"
         )
-    val_utm = UtmMapper.validate_utm(
-        session=session,
-        campaign=data.campaign,
-        source=data.source,
-        medium=data.medium,
-        user_id=auth_user.get("user_id"),
-    )
-    if val_utm:
-        return val_utm
     data = data.model_dump()
     data.update({"id": id, "user_id": auth_user.get("user_id")})
     return UtmMapper.update(session=session, data=data)
