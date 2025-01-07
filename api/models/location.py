e1c64a1f80cc9ea210a4ab9d5647f4011523bae3
quantum-ernest <ernestasare2411@gamil.com>
2025-01-07 15:32:55 +0000

    api: refactor create location

--- api/models/location.py
+++ api/models/location.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from models import Base
 from sqlalchemy import insert, select
 from sqlalchemy.orm import Mapped, Session, relationship
@@ -5,31 +7,26 @@ from utils import extract_location
 
 
 class LocationMapper(Base):
-    continent: Mapped[str | None]
-    country: Mapped[str | None]
-    region: Mapped[str | None]
-    city: Mapped[str | None]
-
+    continent: Mapped[Optional[str]]
+    country: Mapped[Optional[str]]
+    region: Mapped[Optional[str]]
+    city: Mapped[Optional[str]]
     click: Mapped["ClickMapper"] = relationship(back_populates="location")
 
     @classmethod
     def create(cls, session: Session, **kwargs):
         ip_address = kwargs.get("ip_address")
         location_data = extract_location(ip_address)
-        query = select(cls)
-        if location_data:
-            if location_data.get("continent"):
-                query = query.filter_by(continent=location_data["continent"])
-            if location_data.get("country"):
-                query = query.filter_by(country=location_data["country"])
-            if location_data.get("region"):
-                query = query.filter_by(region=location_data["region"])
-            if location_data.get("city"):
-                query = query.filter_by(city=location_data["city"])
-            record = session.scalars(query).first()
-            return (
-                record
-                if record
-                else session.scalars(insert(cls).returning(cls), location_data).first()
+        record = session.scalars(
+            select(cls).filter_by(
+                continent=location_data["continent"],
+                country=location_data["country"],
+                region=location_data["region"],
+                city=location_data["city"],
             )
-        return None
+        ).first()
+        return (
+            record
+            if record
+            else session.scalars(insert(cls).returning(cls), location_data).first()
+        )
