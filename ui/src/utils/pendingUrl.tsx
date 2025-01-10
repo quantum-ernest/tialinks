cb45b9f2a4308760ffdd48b8197565cf8e826ece
quantum-ernest <ernestasare2411@gamil.com>
2025-01-10 22:27:29 +0000

    ui: add pending url getter and setter functions

---
+++ ui/src/utils/pendingUrl.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+export const setPendingUrl = (pendingUrl: string) => {
+    if (typeof window !== 'undefined') {
+        localStorage.setItem('pendingUrl', pendingUrl);
+    }
+};
+
+export const getPendingUrl = (): string | null => {
+    if (typeof window !== 'undefined') {
+        const pendingUrl = localStorage.getItem('pendingUrl');
+        localStorage.removeItem('pendingUrl');
+        return pendingUrl
+    }
+    return null;
+};
