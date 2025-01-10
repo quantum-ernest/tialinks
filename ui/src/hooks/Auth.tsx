04f3c825a1802af72f4b65462dec457d8f716b84
quantum-ernest <ernestasare2411@gamil.com>
2025-01-10 22:25:52 +0000

    ui: create pending url after login

--- ui/src/hooks/Auth.tsx
+++ ui/src/hooks/Auth.tsx
@@ -3,6 +3,8 @@ import {useEffect, useState} from "react";
 import {displayNotifications} from "@/utils/notifications";
 import {isTokenValid, removeToken, setToken, setUserObject} from "@/utils/auth";
 import {useRouter} from "next/navigation";
+import {getPendingUrl} from "@/utils/pendingUrl";
+import {useLinks} from "@/hooks/Links";
 
 const apiUrl = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
 
@@ -12,19 +14,18 @@ export const useAuth = () => {
     const [step, setStep] = useState<'email' | 'otp'>('email');
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const router = useRouter();
-
+    const {createLink} = useLinks();
     useEffect(() => {
         checkAuth();
     }, []);
 
     const checkAuth = () => {
-        const isValid = isTokenValid();
-        setIsAuthenticated(isValid);
-        if (!isValid) {
-            router.push('/login');
+        const isValidToken = isTokenValid();
+        setIsAuthenticated(isValidToken);
+        if (!isValidToken) {
             removeToken();
         }
-        return isValid;
+        return isValidToken;
     };
 
 
@@ -51,6 +52,7 @@ export const useAuth = () => {
     }
 
     const submitOTP = async (email: string, otp: string) => {
+        const pendingUrl = getPendingUrl()
         try {
             setLoading(true);
             const response = await fetch(apiUrl + '/api/auth/otp/email/login', {
@@ -63,9 +65,14 @@ export const useAuth = () => {
             }
             const data = await response.json()
             setToken(data.token);
-            console.log(data.token)
             setUserObject(JSON.stringify(data.user))
             setIsAuthenticated(true);
+            if (pendingUrl) {
+                const newLink = await createLink(pendingUrl);
+                if (newLink) {
+                    openNotification('success', 'Short link generated successfully.', newLink.generated_url)
+                }
+            }
             router.push('/dashboard');
         } catch (error) {
             // @ts-ignore
