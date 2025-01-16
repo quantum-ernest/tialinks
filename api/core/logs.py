9b3167ba8b39254ff445020d82ab307785ae2a04
quantum-ernest <ernestasare2411@gamil.com>
2025-01-16 02:59:39 +0000

    api: add custom formater for prod mode

--- api/core/logs.py
+++ api/core/logs.py
@@ -2,12 +2,19 @@ from uvicorn.logging import AccessFormatter
 
 from core import process_time_ctx
 import logging
+from logging import Formatter
 
 
-class CustomFormatter(AccessFormatter):
+class UvicornCustomFormatter(AccessFormatter):
     def formatMessage(self, record: logging.LogRecord) -> str:
         record.process_time = record.process_time if record.process_time else 0.0
         return super().formatMessage(record)
+
+
+class GunicornCustomFormatter(Formatter):
+    def format(self, record):
+        record.process_time = record.process_time if record.process_time else 0.0
+        return super().format(record)
 
 
 old_factory = logging.getLogRecordFactory()
