5d28afd0ddeb5c66b544a130b202238f413d05b0
quantum-ernest <ernestasare2411@gamil.com>
2025-01-20 23:41:09 +0000

    fix(api): unknown command in gunicorn

--- api/api_starter.sh
+++ api/api_starter.sh
@@ -2,4 +2,4 @@
 
 alembic upgrade head
 
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --host 0.0.0.0 --reload --port 8000 --log-config gunicorn_logging.conf
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8000 --log-config gunicorn_logging.conf
